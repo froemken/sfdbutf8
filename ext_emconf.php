a736c16579d5b8dc66ac604830827521825ed9c7
Stefan Froemken <froemken@gmail.com>
2022-01-25 20:55:07 +0100

    Add TYPO3 11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'DB_UTF8',
     'description' => 'With this extension you can set all tables and columns to UTF-8 collation (no converting)',
     'category' => 'be',
-    'version' => '1.2.4',
+    'version' => '2.0.0',
     'module' => 'mod1',
     'state' => 'stable',
     'author' => 'Stefan Froemken',
@@ -12,7 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.29-10.4.99',
+            'php' => '7.2.0-8.1.0',
+            'typo3' => '9.5.29-11.5.99',
         ],
         'conflicts' => [
         ],
